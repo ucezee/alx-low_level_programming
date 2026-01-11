9d37a42bfa4e490540fd7e6c888b8f07e1ff5542
Uchenna <uunigwe72@gmail.com>
2026-01-11 00:44:03 -0800

    added file 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: 0 (always Success)
+ */
+int main(void)
+{
+	int alpha;
+
+	alpha = '0';
+
+	while (alpha <= '9')
+	{
+		putchar(alpha);
+		if (alpha != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		else
+		{
+			putchar('\n');
+		}
+		alpha++;
+	}
+
+	return (0);
+}
