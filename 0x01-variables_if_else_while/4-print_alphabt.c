ea8e6b760a0d2c37ed036911ccec8f399a3fef06
Uchenna <uunigwe72@gmail.com>
2026-01-10 23:39:19 -0800

    added file 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: 0 (always Success)
+ */
+int main(void)
+{
+	char alpha;
+
+	alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		if ((alpha != 'q') && (alpha != 'e'))
+		{
+			putchar(alpha);
+		}
+		if (alpha == 'z')
+		{
+			putchar('\n');
+		}
+		alpha++;
+	}
+
+	return (0);
+}
