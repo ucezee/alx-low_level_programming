4f4a9b776e7f461c2d6e61b6c26f36c3db8afe82
Uchenna <uunigwe72@gmail.com>
2026-01-12 01:48:06 -0800

    added file 101-print-comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: 0 (always Success)
+ */
+int main(void)
+{
+	int int1;
+	int int2;
+	int int3;
+
+	int1 = 48;
+	int2 = 48;
+	int3 = 48;
+
+	while (int1 <= 57)
+	{
+		while (int2 <= 57)
+		{
+			while (int3 <= 57)
+			{
+				if ((int1 < int2) && (int2 < int3))
+				{
+					if ((int1 == 48) && (int2 == 49) && (int3 == 50))
+					{
+					}
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					putchar (int1);
+					putchar (int2);
+					putchar (int3);
+
+				}
+				int3++;
+			}
+			int3 = 48;
+
+			int2++;
+		}
+		int2 = 48;
+
+		int1++;
+	}
+	putchar ('\n');
+
+	return (0);
+}
