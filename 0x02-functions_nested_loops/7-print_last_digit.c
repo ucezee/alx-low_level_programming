775faa4f31d391437ecaaa6693d2d8852aff56dd
Uchenna <uunigwe72@gmail.com>
2026-01-14 04:34:50 -0800

    added 7-print-last-digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_last_digit - entry point
+ *
+ * @c: integer to be evaluated
+ *
+ * Return: onsuccess last digit of evaluated integer
+ */
+int print_last_digit(int c)
+{
+	if (c < 0)
+	{
+		_putchar('0' + (-c % 10));
+		return (-c % 10);
+	}
+	else
+	{
+		_putchar('0' + (c % 10));
+		return (-c % 10);
+	}
+}
