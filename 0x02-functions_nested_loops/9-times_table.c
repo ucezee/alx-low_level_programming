d8849cd309e597040fef6cf4827834255d839017
Uchenna <uunigwe72@gmail.com>
2026-01-15 02:54:27 -0800

    added 9-times_table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * times_table - Entry
+ *
+ * Return: Null
+ */
+void times_table(void)
+{
+	int f, s, res; 
+
+	f = 0;
+	while (f < 10)
+	{
+		s = 0;
+		while (s < 10)
+		{
+			res = f * s;
+			if ((s != 0))
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (res < 10)
+			{
+				_putchar(' ');
+				_putchar('0' + res);
+			}
+			else
+			{
+				_putchar('0' + (res / 10));
+				_putchar('0' + (res % 10));
+			}
+			s++;
+		}
+		_putchar('\n');
+		f++;
+	}
+}
