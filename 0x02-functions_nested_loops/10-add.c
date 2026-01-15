12c1d19dedc7b2ad441e78dbf3a3cbb09e0af31d
Uchenna <uunigwe72@gmail.com>
2026-01-15 03:09:38 -0800

    added file 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+/**
+ * add - function to add two numbers
+ *
+ * @a: first number to be added
+ * @b: second number to be added
+ *
+ * Return: result of computation
+ */
+int add(int a, int b)
+{
+	return(a + b);
+}
+ 
